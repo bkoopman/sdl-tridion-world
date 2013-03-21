d780ac9dc800158a656ca824978bde953f4be69a
bkoopman@sdl.com <bkoopman@sdl.com@40952634-5a44-2b49-8168-114e4f8c33c5>
2013-03-21 11:25:52 +0000

    Added 2013 Flickr ECL provider

---
+++ Flickr ECL Provider/trunk/Flickr ECL Provider/FlickrPhotoSet.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Example.EclProvider.Api;
+using Tridion.ExternalContentLibrary.V2;
+
+namespace Example.EclProvider
+{
+    /// <summary>
+    /// Represents a Flickr (Photo) Set with all details loaded. 
+    /// </summary>
+    public class FlickrPhotoSet : ListItem, IContentLibraryItem
+    {
+        public FlickrPhotoSet(int publicationId, FlickrInfo info) : base(publicationId, info)
+        {
+            // if info needs to be fully loaded, do so here
+        }
+
+        public bool CanGetViewItemUrl
+        {
+            get { return true; }
+        }
+
+        public bool CanUpdateMetadataXml
+        {
+            get { return false; }
+        }
+
+        public bool CanUpdateTitle
+        {
+            get { return false; }
+        }
+
+        public DateTime? Created
+        {
+            get { return Info.Created; }
+        }
+
+        public string CreatedBy
+        {
+            get { return Provider.Flickr.UserId; }
+        }
+
+        public string MetadataXml
+        {
+            get { return null; }
+            set { throw new NotSupportedException(); }
+        }
+
+        public ISchemaDefinition MetadataXmlSchema
+        {
+            get { return null; }
+        }
+
+        public string ModifiedBy
+        {
+            get { return CreatedBy; }
+        }
+
+        public IEclUri ParentId
+        {
+            get
+            {
+                // return mountpoint uri (we only have folders in the top level)
+                return Provider.HostServices.CreateEclUri(Id.PublicationId, Id.MountPointId);
+            }
+        }
+
+        public IContentLibraryItem Save(bool readback)
+        {
+            // as saving isn't supported, the result of saving is always the item itself
+            return readback ? this : null;
+        }
+    }
+}
---
+++ Flickr ECL Provider/trunk/Flickr ECL Provider/Mountpoint.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Example.EclProvider.Api;
+using Tridion.ExternalContentLibrary.V2;
+
+namespace Example.EclProvider
+{
+    public class Mountpoint : IContentLibraryContext
+    {
+        public bool CanGetUploadMultimediaItemsUrl(int publicationId)
+        {
+            return true;
+        }
+
+        public bool CanSearch(int publicationId)
+        {
+            return false;
+        }
+
+        public IList<IContentLibraryListItem> FindItem(IEclUri eclUri)
+        {
+            // return null so we force it to call GetItem(IEclUri)
+            return null;
+        }
+
+        public IFolderContent GetFolderContent(IEclUri parentFolderUri, int pageIndex, EclItemTypes itemTypes)
+        {
+            List<IContentLibraryListItem> items = new List<IContentLibraryListItem>();
+
+            if (parentFolderUri.ItemType == EclItemTypes.MountPoint && itemTypes.HasFlag(EclItemTypes.Folder))
+            {
+                // get photosets
+                foreach (FlickrInfo info in Provider.Flickr.GetPhotoSets())
+                {
+                    items.Add(new ListItem(parentFolderUri.PublicationId, info));
+                }
+            }
+            // only return files if they are requested (itemTypes is EclItemTypes.File))
+            else if (parentFolderUri.ItemType == EclItemTypes.Folder && parentFolderUri.SubType == "set" && itemTypes.HasFlag(EclItemTypes.File))
+            {
+                // get photos
+                foreach (FlickrInfo info in Provider.Flickr.GetPhotosInSet(parentFolderUri.ItemId))
+                {
+                    //items.Add(new FlickrPhoto(parentFolderUri.PublicationId, info));
+                    items.Add(new ListItem(parentFolderUri.PublicationId, info));
+                }
+            }
+
+            return Provider.HostServices.CreateFolderContent(parentFolderUri, items, CanGetUploadMultimediaItemsUrl(parentFolderUri.PublicationId), CanSearch(parentFolderUri.PublicationId));
+        }
+
+        public IContentLibraryItem GetItem(IEclUri eclUri)
+        {
+            if (eclUri.ItemType == EclItemTypes.File && eclUri.SubType == "img")
+            {
+                // format of photo item id: [flickr photo id]_[flickr photo secret]_[flickr photo set id]
+                string[] ids = eclUri.ItemId.Split('_');
+                return new FlickrPhoto(eclUri.PublicationId, Provider.Flickr.GetPhotoInfo(ids[0], ids[1], ids[2]));
+            }
+
+            if (eclUri.ItemType == EclItemTypes.Folder && eclUri.SubType == "set")
+            {
+                return new FlickrPhotoSet(eclUri.PublicationId, Provider.Flickr.GetPhotoSetInfo(eclUri.ItemId));
+            }
+
+            throw new NotSupportedException();
+        }
+
+        public IList<IContentLibraryItem> GetItems(IList<IEclUri> eclUris)
+        {
+            List<IContentLibraryItem> items = new List<IContentLibraryItem>();
+            IEnumerable<string> uniquePhotoIds = (from uri in eclUris
+                                                  where uri.ItemType == EclItemTypes.File && uri.SubType == "img"
+                                                  select uri.ItemId).Distinct();
+            foreach (string id in uniquePhotoIds)
+            {
+                string itemId = id;
+                var urisForPhoto = from uri in eclUris
+                                   where uri.ItemType == EclItemTypes.File && uri.SubType == "img" && uri.ItemId == itemId
+                                   select uri;
+
+                foreach (IEclUri eclUri in urisForPhoto)
+                {
+                    items.Add(GetItem(eclUri));
+                }
+            }
+            return items;
+        }
+
+        public byte[] GetThumbnailImage(IEclUri eclUri, int maxWidth, int maxHeight)
+        {
+            // only return thumbnails for the actual photos
+            if (eclUri.ItemType == EclItemTypes.File && eclUri.SubType == "img")
+            {
+                // format of photo item id: [flickr photo id]_[flickr photo secret]_[flickr photo set id]
+                string[] ids = eclUri.ItemId.Split('_');
+                FlickrInfo photo = Provider.Flickr.GetPhotoInfo(ids[0], ids[1], ids[2]);
+
+                WebClient webClient = new WebClient();
+                byte[] thumbnailData = webClient.DownloadData(Flickr.GetPhotoUrl(photo, maxWidth));
+                using (MemoryStream ms = new MemoryStream(thumbnailData, false))
+                {
+                    return Provider.HostServices.CreateThumbnailImage(maxWidth, maxHeight, ms, Flickr.MaxWidth, Flickr.MaxHeight, null);
+                }
+            }
+            return null;
+        }
+
+        public string GetUploadMultimediaItemsUrl(IEclUri parentFolderUri)
+        {
+            if (parentFolderUri.ItemType == EclItemTypes.MountPoint)
+            {
+                return Provider.Flickr.GetPhotoSetPageUrl(null);
+            }
+
+            if (parentFolderUri.ItemType == EclItemTypes.Folder && parentFolderUri.SubType == "set")
+            {
+                return Provider.Flickr.GetPhotoSetPageUrl(parentFolderUri.ItemId);
+            }
+
+            throw new NotSupportedException();
+        }
+
+        public string GetViewItemUrl(IEclUri eclUri)
+        {
+            if (eclUri.ItemType == EclItemTypes.File && eclUri.SubType == "img")
+            {
+                // format of photo item id: [flickr photo id]_[flickr photo secret]_[flickr photo set id]
+                string[] ids = eclUri.ItemId.Split('_');
+                return Provider.Flickr.GetPhotoPageUrl(ids[0]);
+            }
+
+            throw new NotSupportedException();
+        }
+
+        public string IconIdentifier
+        {
+            get { return "flickr"; }
+        }
+
+        public IFolderContent Search(IEclUri contextUri, string searchTerm, int pageIndex, int numberOfItems)
+        {
+            throw new NotSupportedException();
+        }
+
+        public string Dispatch(string command, string payloadVersion, string payload, out string responseVersion)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void StubComponentCreated(IEclUri eclUri, string tcmUri)
+        {
+        }
+
+        public void StubComponentDeleted(IEclUri eclUri, string tcmUri)
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
