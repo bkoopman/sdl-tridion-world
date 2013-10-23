bbd71bbc631e403378f87da88a8930dad0bd7b26
jan.horsman <jan.horsman@40952634-5a44-2b49-8168-114e4f8c33c5>
2013-10-23 11:43:57 +0000

    - Flickr API client in Api/Flickr.cs throws exception when the Flickr service does not reply with success - The Mountpoint shows a error message item (ErrorListItem.cs) if the Flickr user does not have any public photo sets
    This commit is inspired by the answers op this question on Tridion Stackexchange http://tridion.stackexchange.com/questions/3097/raising-an-error-from-a-ecl-provider

---
+++ Flickr ECL Provider/trunk/Flickr ECL Provider/ErrorListItem.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Example.EclProvider.Api;
+using Example.EclProvider.Extensions;
+using Tridion.ExternalContentLibrary.V2;
+
+namespace Example.EclProvider
+{
+    /// <summary>
+    /// Represents the basic properties of a Flickr (Photo) Set. 
+    /// </summary>
+    public class ErrorListItem : IContentLibraryListItem
+    {
+        private readonly IEclUri _id;
+        private readonly string _errorMessage;
+
+        public ErrorListItem(int publicationId, string errorMessage)
+        {
+            _id = Provider.HostServices.CreateEclUri(publicationId, Provider.MountPointId, "error", DisplayTypeId, EclItemTypes.Folder);
+            _errorMessage = errorMessage;
+        }
+
+        // for folders only
+        public bool CanGetUploadMultimediaItemsUrl
+        {
+            get { return false; }
+        }
+
+        // for folders only
+        public bool CanSearch
+        {
+            get { return false; }
+        }
+
+        public string DisplayTypeId
+        {
+            get { return "error"; }
+        }
+
+        public string IconIdentifier
+        {
+            //todo return a error icon
+            get { return "error"; }
+        }
+
+        public IEclUri Id
+        {
+            get { return _id; }
+        }
+
+        public bool IsThumbnailAvailable
+        {
+            get { return false; }
+        }
+
+        public DateTime? Modified
+        {
+            get { return null; }
+        }
+
+        public string ThumbnailETag
+        {
+            get { return null; }
+        }
+
+        public string Title
+        {
+            get { return _errorMessage; }
+            set { throw new NotSupportedException(); }
+        }
+
+        // allow override of dispatch
+        public virtual string Dispatch(string command, string payloadVersion, string payload, out string responseVersion)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
