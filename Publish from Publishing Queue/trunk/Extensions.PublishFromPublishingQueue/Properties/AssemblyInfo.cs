23a6dd4f81906d427f45e645e6915b48877ed91c
bkoopman@sdl.com <bkoopman@sdl.com@40952634-5a44-2b49-8168-114e4f8c33c5>
2013-12-04 18:01:36 +0000

    Added 2013 SP1 Publish from Publishing Queue extension

---
+++ Publish from Publishing Queue/trunk/Extensions.PublishFromPublishingQueue/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Extensions.PublishFromPublishingQueue")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SDL WCMS Division")]
+[assembly: AssemblyProduct("Extensions.PublishFromPublishingQueue")]
+[assembly: AssemblyCopyright("Copyright © SDL 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("25c324a6-38d1-4493-9b79-2a92ed31c51f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
