7461683f3ef0041534a0c822fd2be461ee66a295
bkoopman@sdl.com <bkoopman@sdl.com@40952634-5a44-2b49-8168-114e4f8c33c5>
2014-03-04 15:21:40 +0000

    added Child Publications Only Resolver and Empty Publish Transactions Event Handler

---
+++ Empty Publish Transactions Event Handler/Empty Publish Transactions Event Handler/EventHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Xml;
+using Tridion;
+using Tridion.ContentManager.Extensibility;
+using Tridion.ContentManager.Extensibility.Events;
+using Tridion.ContentManager.Publishing;
+
+namespace Example
+{
+    /// <summary>
+    /// Change the state Success of an empty Publish Transaction to Warning
+    /// </summary>
+    [TcmExtension("EmptyPublishTransactionsEventHandlerExtension")]
+    public class EventHandler : TcmExtension
+    {
+        /// <summary>
+        /// R6 Delta XML PublishTransactionData
+        /// </summary>
+        private const string DeltaXml = "<tcm:PublishTransactionData xmlns:tcm=\"{0}\"><tcm:Id>{1}</tcm:Id><tcm:Information>{2}</tcm:Information><tcm:State>{3}</tcm:State></tcm:PublishTransactionData>";
+
+        /// <summary>
+        /// Warning message for empty Publish Transaction
+        /// </summary>
+        private const string Message = "This Publish Transaction contains 0 (zero) items.";
+
+        /// <summary>
+        /// Subscribe to PublishTransaction Save (Initiated) event 
+        /// </summary>
+        public EventHandler()
+        {
+            EventSystem.Subscribe<PublishTransaction, SaveEventArgs>(PublishTransactionSaveAction, EventPhases.Initiated);
+        }
+
+        private void PublishTransactionSaveAction(PublishTransaction subject, SaveEventArgs args, EventPhases phases)
+        {
+            // after the publisher has used the Resolve Engine to figure out what to publish/render or unpublish, 
+            // it reflects the resolved results in PublishTransaction.PublishContexts[0].ProcessedItems
+            // currently there will always be exactly one PublishContext per PublishTransaction
+            if (subject.PublishContexts[0].ProcessedItems.Count == 0 && subject.State.Equals(PublishTransactionState.Success))
+            {
+                // change state to warning and inform that this publish transaction had 0 items 
+                // could change state by setting property PublishTransaction.State, 
+                // but the property PublishTransaction.Information is readonly, so let's use IdentifiableObject.Update 
+                XmlDocument delta = new XmlDocument();
+                delta.LoadXml(string.Format(DeltaXml, Constants.TcmR6Namespace, subject.Id, Message, PublishTransactionState.Warning));
+                subject.Update(delta.DocumentElement);
+            }
+        }
+    }
+}
