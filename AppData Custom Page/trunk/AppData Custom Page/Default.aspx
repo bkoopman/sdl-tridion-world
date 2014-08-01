cd402a1a2851653a5e12648fbbd49cf746673272
bkoopman@sdl.com <bkoopman@sdl.com@40952634-5a44-2b49-8168-114e4f8c33c5>
2014-08-01 12:07:02 +0000

    added AppData Custom Page example

---
+++ AppData Custom Page/trunk/AppData Custom Page/Default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" validateRequest="false" CodeBehind="Default.aspx.cs" Inherits="Example.CustomPage.Default" %>
+
+<!DOCTYPE html>
+<html lang="en">
+<head id="Head1" runat="server">
+    <title>Semantic Mapping</title>
+    <link rel="stylesheet" type="text/css" href="Theme/Styles.css">
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="ScriptManager1" EnablePartialRendering="true" runat="server" />
+        <div id="LocationBar" class="control flisttopbar">
+            <div class="location">
+                <div id="Location" class="label" style="display: block;">Semantic Mapping</div>
+                <div id="ReloadBtn" class="tridion button refresh" tabindex="1" onclick="location.reload();">
+                    <span class="text">&nbsp;</span>
+                </div>
+            </div>
+        </div>
+        <div id="Content" class="stack-calc" style="height: 216px;">
+            <table class="nolines">
+                <tr>
+                    <td><strong>Vocabularies</strong></td>
+                    <td><strong>Schema mapping</strong></td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:TextBox runat="server" ID="AppData" TextMode="MultiLine" Rows="6" Width="450"></asp:TextBox>
+                    </td>
+                    <td>
+                        <asp:DropDownList runat="server" ID="SchemaList" OnSelectedIndexChanged="SchemaListChanged" AutoPostBack="True"></asp:DropDownList>
+                        <asp:TextBox runat="server" ID="TypeOf" Width="300"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button runat="server" ID="UpdateVocabs" Text="Update" CssClass="button" OnClick="UpdateVocabsClick" />
+                        <asp:Label runat="server" ID="VocabsLabel"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:Button runat="server" ID="UpdateSchema" Text="Update" CssClass="button" OnClick="UpdateSchemaClick" />
+                        <asp:Label runat="server" ID="SchemaLabel"></asp:Label>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ AppData Custom Page/trunk/AppData Custom Page/Default.aspx.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.ServiceModel;
+using System.Text;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using Tridion.ContentManager.CoreService.Client;
+
+namespace Example.CustomPage
+{
+    public partial class Default : Page
+    {
+        #region constants
+        // services endpoint and binding
+        private readonly NetTcpBinding _binding = new NetTcpBinding
+        {
+            MaxReceivedMessageSize = 2147483647,
+            ReaderQuotas = new XmlDictionaryReaderQuotas
+            {
+                MaxStringContentLength = 2147483647,
+                MaxArrayLength = 2147483647
+            }
+        };
+        #endregion
+
+        private EndpointAddress _coreServiceEndpoint;
+        private string _vocabulariesAppId;
+        private string _typeOfAppId;
+        private string _schemaPublicationUri;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            _coreServiceEndpoint = new EndpointAddress(WebConfigurationManager.AppSettings["EndpointAddress"]);
+            _vocabulariesAppId = WebConfigurationManager.AppSettings["VocabulariesAppId"];
+            _typeOfAppId = WebConfigurationManager.AppSettings["TypeOfAppId"];
+            _schemaPublicationUri = WebConfigurationManager.AppSettings["SchemaPublicationUri"];
+
+            if (!IsPostBack)
+            {
+                var client = new SessionAwareCoreServiceClient(_binding, _coreServiceEndpoint);
+                client.Impersonate(HttpContext.Current.User.Identity.Name);
+
+                // load vocabularies appdata
+                ApplicationData appData = client.ReadApplicationData(null, _vocabulariesAppId);
+                if (appData != null)
+                {
+                    AppData.Text = Encoding.Unicode.GetString(appData.Data);
+                }
+                else
+                {
+                    // load default xml
+                    AppData.Text = "<vocabularies>\n  <vocabulary prefix=\"s\" name=\"http://schema.org\"/>\n</vocabularies>";
+                }
+
+                // load schemas
+                var filter = new RepositoryItemsFilterData { ItemTypes = new[] { ItemType.Schema }, Recursive = true };
+                var schemas = client.GetList(_schemaPublicationUri, filter);
+                foreach (var schema in schemas)
+                {
+                    SchemaList.Items.Add(new ListItem(schema.Title, schema.Id));
+                }
+
+                // load appdata for first schema in the list
+                appData = client.ReadApplicationData(schemas[0].Id, _typeOfAppId);
+                if (appData != null)
+                {
+                    TypeOf.Text = Encoding.Unicode.GetString(appData.Data);
+                }
+
+                Close(client);
+            }
+        }
+
+        protected void SchemaListChanged(object sender, EventArgs e)
+        {
+            var client = new SessionAwareCoreServiceClient(_binding, _coreServiceEndpoint);
+            client.Impersonate(HttpContext.Current.User.Identity.Name);
+
+            ApplicationData appData = client.ReadApplicationData(SchemaList.SelectedItem.Value, _typeOfAppId);
+            if (appData != null)
+            {
+                TypeOf.Text = Encoding.Unicode.GetString(appData.Data);
+            }
+            else
+            {
+                // reset textbox
+                TypeOf.Text = String.Empty;
+            }
+
+            Close(client);
+
+            SchemaLabel.Text = SchemaList.SelectedItem.Value;
+        }
+
+        protected void UpdateSchemaClick(object sender, EventArgs e)
+        {
+            var client = new SessionAwareCoreServiceClient(_binding, _coreServiceEndpoint);
+            client.Impersonate(HttpContext.Current.User.Identity.Name);
+
+            var appData = new ApplicationData
+            {
+                ApplicationId = _typeOfAppId,
+                Data = Encoding.Unicode.GetBytes(TypeOf.Text)
+            };
+            client.SaveApplicationData(SchemaList.SelectedItem.Value, new[] { appData });
+
+            Close(client);
+
+            SchemaLabel.Text = SchemaList.SelectedItem.Value + " saved...";
+        }
+
+        protected void UpdateVocabsClick(object sender, EventArgs e)
+        {
+            var client = new SessionAwareCoreServiceClient(_binding, _coreServiceEndpoint);
+            client.Impersonate(HttpContext.Current.User.Identity.Name);
+
+            var appData = new ApplicationData
+            {
+                ApplicationId = _vocabulariesAppId,
+                Data = Encoding.Unicode.GetBytes(AppData.Text)
+            };
+            client.SaveApplicationData(null, new[] { appData });
+
+            Close(client);
+
+            VocabsLabel.Text = "saved...";
+        }
+
+        private static void Close(ICommunicationObject client)
+        {
+            try
+            {
+                if (client.State != CommunicationState.Faulted)
+                {
+                    client.Close();
+                }
+            }
+            finally
+            {
+                if (client.State != CommunicationState.Closed)
+                {
+                    client.Abort();
+                }
+            }
+        }
+    }
+}
